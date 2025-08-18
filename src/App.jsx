bb9958617f6233eb219c4810916233a3f0fe32e4
Robel Yakob <robelyakobpoll@gmail.com>
2025-08-18 23:56:03 +0300

    tried out some react with sami

--- src/App.jsx
+++ src/App.jsx
@@ -3,31 +3,51 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+
+function countup(){
+  console.log('hellow from submit button')
+}
+
+
+function InputField({par, classname ,inputtype}){
+
+  return (
+    <>
+      <p>{par}</p>
+      <input type={inputtype} className={classname}/>
+    </>
+  )
+
+}
+
+function Button({text, callfunc}){
+  return(
+
+    <button onClick={() =>{
+      
+    }}>
+      {text}
+    </button>
+  )
+}
+
+
+
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <InputField par = 'Username :' classname = 'usernameField' />
+        <InputField par = 'Password :' classname = 'passwordField' inputtype = 'password' />  
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div>
+        <Button  text = 'Login' />
+        <Button text = 'Sign up' />
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
