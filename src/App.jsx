053c4f17e9d0eb63fedd9c8f39ca795950eb47c4
SammyG02 <samuelgosaye02@gmail.com>
2025-08-21 18:32:46 +0300

    used router for signup page.. no signup form yet

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route, Link, useNavigate } from 'react-router-dom';
 import './App.css'
 
 
@@ -23,33 +24,70 @@ function Button({text, callfunc}){
   )
 }
 
-function App() {
-
-
-  const [count, setCount] = useState(0)
-
-  return (
+function Footer({goto}){
+  return(
+    <>
+      <p>Don't have an Account? <Link to={goto} style={{ color: '#007bff', textDecoration: 'none' }}>
+          Sign up
+        </Link></p>
+      
+    </>
+    
+  )
+}
+function SignInPage(){
+  return(
     <div  className='login-card'>
-
       <h1>Welcome!</h1>
       
       <div className='login-inp'>
-        <InputField par = 'Username :' classname = 'usernameField' inputtype= 'text' />
-        <InputField par = 'Password :' classname = 'passwordField' inputtype = 'password' />  
+        <InputField par = 'Username' classname = 'usernameField' inputtype= 'text' />
+        <InputField par = 'Password' classname = 'passwordField' inputtype = 'password' />  
       </div>
       <div className="login-opt">
         <div className="rememberme">
           <input type="checkbox" name="remember" id="" />
-          <span>Remember me!</span>
+          <span>Remember Me!</span>
         </div>
         <p><a href="#blank">Forgot password?</a></p>
       </div>
       <div className='login-btns'>
         <Button  text = 'LOG IN' />
       </div>
-      {/* <p>{count}</p> */}
+      <div className='create-account'>
+        <Footer goto="/signup" />
+      </div>
+    </div>
+  );
+}
+function SignUpPage(){
+    return(
+    <div  className='login-card'>
+      <h1>Welcome to signup!</h1>
+      
+      <Footer goto="/" />
       
     </div>
+  );
+}
+
+
+function App() {
+
+
+  const [count, setCount] = useState(0);
+  
+
+  return (
+    <Router>
+      <div className="app-container">
+        <Routes>
+          <Route path="/" element={<SignInPage />} />
+          <Route path="/signup" element={<SignUpPage />} />
+        </Routes>
+      </div>
+    </Router>
+    
   )
 }
 
