fd7e2c7871034667103c23d69d55f084e8473d8f
SammyG02 <samuelgosaye02@gmail.com>
2025-08-27 11:58:06 +0300

    to see create project

--- src/main.jsx
+++ src/main.jsx
@@ -6,14 +6,16 @@ import App from './App.jsx'
 import Home from './pages/Home.jsx';
 import Signing from './pages/Signing'
 import Registration from './pages/registration.jsx'
+import CreateProjectForm from './pages/CreateProject.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter> 
-      <App />
+      {/* <App /> */}
       {/* <Home/> */}
       {/* <Signing /> */}
       {/* <Registration/> */}
+      <CreateProjectForm />
     </BrowserRouter>
   </StrictMode>,
 )
