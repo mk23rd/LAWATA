f29ec4cc348d2fba18c9a3158fed2cba2f97c3ad
Robel <robelyakobii@gmail.com>
2025-09-02 15:56:56 +0300

    "reverted"

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'; // Add this
+import { BrowserRouter } from 'react-router-dom';
 import './index.css'
 import App from './App.jsx'
-import Home from './pages/Home.jsx';
-import Signing from './pages/Signing'
-import Registration from './pages/registration.jsx'
+// import Home from './pages/Home.jsx';
+// import Signing from './pages/Signing'
+// import Registration from './pages/registration.jsx'
 // import CreateProject from '/pages/CreateProject';
 
 
